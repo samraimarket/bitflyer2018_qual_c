ef39feb6986ef502ee2f8f855fe8a0f701b3f141
DESKTOP-BGDE0P6\takec <takechikazuma@gmail.com>
2018-06-04 23:06:07 +0900

    6-4_last

--- main.cpp
+++ main.cpp
@@ -6,28 +6,33 @@ size_t N, D;
 inline
 size_t find(size_t min, size_t mid, size_t max, size_t const end) {
 
-    if (mid == max || min == mid) return mid;
 
-    uint64_t last_p = X[end - 1];
+    uint64_t last_p = X[end];
     if ((last_p - X[mid]) <= D) {
 
         // check next one
-        if ((last_p - X[mid + 1]) > D) return mid;
+        if ((last_p - X[mid - 1]) > D || (min == mid)) return mid;
         
         max = mid;
         mid = (min + mid) / 2;
+
     } else {
-        // Go to far
+
+       if (mid == (max - 1)) return mid;
         min = mid;
         mid = (mid + max) / 2;
     }
+
     return find(min, mid, max, end);
 }
 
 inline
 size_t startFind(size_t start, size_t end) {
-    if ((X[end] - X[end - 1]) > D) return end;
-    size_t ret = find(start, (start + end) / 2, end - 1, end);
+    if ((X[end] - X[end - 1]) > D) {
+        cout << end << endl;
+        return end;
+    }
+    size_t ret = find(start, (start + end) / 2, end, end);
     return ret;
 }
 int main() {
@@ -45,9 +50,14 @@ int main() {
         if (j == k) continue;
 
         size_t i = startFind(0, j);
-        if (j == i) continue;
+        if (j == i || ((X[k] - X[i]) <= D)) continue;
 
-        ret += (k - j) * (j - i);
+        uint64_t temp = 0;
+        for (size_t a = k - 1; j <= a; a--) {
+            temp += a - i;     
+        }
+        ret += temp;
+        cout << i << ',' << j << ',' << k << '.' << temp <<endl;
     }
     cout << ret << endl;
     return 0;
