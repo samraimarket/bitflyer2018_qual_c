f29dea99090741922504305df9a35471b1f22563
DESKTOP-BGDE0P6\takec <takechikazuma@gmail.com>
2018-06-04 20:55:53 +0900

    implemented recursive function.

--- main.cpp
+++ main.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
 using namespace std;
- 
+uint64_t* X;
+size_t N, D;
+
+inline
+size_t find(size_t min, size_t mid, size_t max, size_t const end) {
+
+    if (mid == max || min == mid) return mid;
+
+    uint64_t last_p = X[end - 1];
+    if ((last_p - X[mid]) <= D) {
+
+        // check next one
+        if ((last_p - X[mid + 1]) > D) return mid;
+        
+        max = mid;
+        mid = (min + mid) / 2;
+    } else {
+        // Go to far
+        min = mid;
+        mid = (mid + max) / 2;
+    }
+    return find(min, mid, max, end);
+}
+
+inline
+size_t startFind(size_t start, size_t end) {
+    if ((X[end - 1] - X[start]) <= D) return end;
+    return find(start, (start + end) / 2, end, end);
+}
 int main() {
  
-    size_t N, D;
     cin >> N >> D;
  
-    uint64_t X[N];
+    uint64_t array[N];
+    X = array;
  
     for (size_t i = 0; i < N; i++) cin >> X[i];
  
     size_t ret = 0;
     for (size_t k = 2; k < N; k++) {
-        size_t M = (k - 1);
-        for (size_t i = 0; i < M; i++) {
-            if((X[k] - X[i]) <= D) break;
-            for (size_t j = i; j < k; j++) {
-                if ((X[j] - X[i]) > D) break;
-                if ((X[k] - X[j]) <= D) ret++;
-            }
-        }
+        size_t j = startFind(1, k);
+        if (j == k) continue;
+
+        size_t i = startFind(0, j);
+        if (j == i) continue;
+
+        ret += (k - j) * (j - i);
     }
     cout << ret << endl;
     return 0;
