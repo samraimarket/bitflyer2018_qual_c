3c024921a11099e20977d20fb5d82b2f3dcb3fea
DESKTOP-BGDE0P6\takec <takechikazuma@gmail.com>
2018-06-06 00:07:10 +0900

    tests_passed

--- main.cpp
+++ main.cpp
@@ -1,53 +1,51 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 uint64_t* X;
+uint64_t* Y;
 size_t N, D;
 
-inline
-size_t find(size_t min, size_t mid, size_t max, size_t const end) {
+inline 
+void inputRangeData(size_t const i) {
+    
+    Y[i] = 0;
+    if (0 == i) return;
 
-    if (mid == max || min == mid) return mid;
-
-    uint64_t last_p = X[end - 1];
-    if ((last_p - X[mid]) <= D) {
-
-        // check next one
-        if ((last_p - X[mid + 1]) > D) return mid;
-        
-        max = mid;
-        mid = (min + mid) / 2;
-    } else {
-        // Go to far
-        min = mid;
-        mid = (mid + max) / 2;
+    for (size_t j =  i - 1; (X[i] - X[j]) <= D; j--) {
+        Y[i]++;
+        if (j == 0) break;
     }
-    return find(min, mid, max, end);
+    cout << i << ',' << Y[i] << endl;
 }
 
-inline
-size_t startFind(size_t start, size_t end) {
-    if ((X[end] - X[end - 1]) > D) return end;
-    size_t ret = find(start, (start + end) / 2, end - 1, end);
-    return ret;
-}
 int main() {
  
     cin >> N >> D;
  
     uint64_t array[N];
     X = array;
- 
-    for (size_t i = 0; i < N; i++) cin >> X[i];
- 
+    
+    uint64_t array2[N];
+    Y = array2;
+
+    for (size_t i = 0; i < N; i++) {
+         cin >> X[i];
+         inputRangeData(i);
+    }
+
     size_t ret = 0;
-    for (size_t k = 2; k < N; k++) {
-        size_t j = startFind(1, k);
-        if (j == k) continue;
+    for (size_t k = N - 1; 2 <= k; k--) {
+        for (size_t j = k - 1; 1 <= j ; j--) {
+            if ((X[k] - X[j]) > D) break;
+            size_t i_min = j - Y[j];
 
-        size_t i = startFind(0, j);
-        if (j == i) continue;
+            if ((X[k] - X[i_min]) <= D) continue;
+            size_t i_max = min<uint64_t>(j - 1, k - Y[k] - 1);
+            if (i_min <= i_max) {
+                ret += i_max - i_min + 1;
+            }
 
-        ret += (k - j) * (j - i);
+        }
     }
     cout << ret << endl;
     return 0;
