c813000117302097b8d21ec2b11aa007541a9035
DESKTOP-BGDE0P6\takec <takechikazuma@gmail.com>
2018-06-06 19:45:18 +0900

    implemented memo feature.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-uint64_t *X, *Y;
+uint64_t *X, *Y, *minMemo;
 size_t N, D;
 
 inline 
 void inputRangeData(size_t const i) {
     
     Y[i] = 0;
+    minMemo[i] = N;
     if (0 == i) return;
 
     for (size_t j =  i - 1; (X[i] - X[j]) <= D; j--) {
@@ -35,8 +36,8 @@ inline
 size_t  startFind(size_t start, size_t end) {
 
     if ((X[end] - X[end - 1]) > D) return end;
-    return find(start, (start + end) / 2, end, end);
-
+    if (minMemo[end] == N) minMemo[end] = find(start, (start + end) / 2, end, end);
+    return minMemo[end];
 }
 
 int main() {
@@ -49,13 +50,17 @@ int main() {
     uint64_t array2[N];
     Y = array2;
 
+    uint64_t array3[N];
+    minMemo = array3;
+
     for (size_t i = 0; i < N; i++) {
          cin >> X[i];
          inputRangeData(i);
     }
 
     size_t ret = 0;
-    for (size_t k = N - 1; 2 <= k; k--) {
+    for (size_t k = 2; k < N; k++) {
+
         for (size_t j = startFind(k - Y[k], k); j < k; j++) {
             size_t i_min = j - Y[j];
             size_t const i_max = min<uint64_t>(k - Y[k], j);
